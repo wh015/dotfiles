c969a074be6ce52b50ce72037c63dbf34838cced
Pavel Preblagin <pp90box@gmail.com>
2022-04-19 18:00:21 +0700

    Update zsh
     - Add installation script
     - Update .zshrc

---
+++ zsh/install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+OMZDIR=$(realpath ~)/.oh-my-zsh
+[ -d ${OMZDIR} ] || {
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+}
+(cd ${OMZDIR} && git pull)
+
+cp .zshrc ~
+
+read -p "Set zsh as default shell (y/n)?" choice
+case "$choice" in
+    y|Y ) chsh -s /bin/zsh $(whoami);;
+esac
+
